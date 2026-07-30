77a1683ae9d0e8ab7df40fae9c6b8fcaf81bb763
Luis Alejandro Pineda Dávila <luisaleja2809@gmail.com>
2026-07-30 18:07:09 -0500

    Actualizar caché para mostrar el nuevo diseño

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='burbujas-amor-v2';
+const CACHE='burbujas-amor-v3';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 
 self.addEventListener('install',event=>{
@@ -7,17 +7,31 @@ self.addEventListener('install',event=>{
 });
 
 self.addEventListener('activate',event=>{
-  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
+  );
   self.clients.claim();
 });
 
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET')return;
+
+  if(event.request.mode==='navigate'){
+    event.respondWith(
+      fetch(event.request).then(response=>{
+        const copy=response.clone();
+        caches.open(CACHE).then(cache=>cache.put('./index.html',copy));
+        return response;
+      }).catch(()=>caches.match('./index.html'))
+    );
+    return;
+  }
+
   event.respondWith(
     caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
       const copy=response.clone();
       caches.open(CACHE).then(cache=>cache.put(event.request,copy));
       return response;
-    }).catch(()=>caches.match('./index.html')))
+    }))
   );
 });
